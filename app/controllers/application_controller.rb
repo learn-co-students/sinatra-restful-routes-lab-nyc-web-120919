fda79794d9a490ada712a2d9e6439598b1163f12
merewelch214 <merewelch@gmail.com>
2020-01-07 16:32:47 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do
+    erb :add_new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    recipe_id = @recipe.id.to_s
+    redirect to ('/recipes/' + recipe_id)
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(name: params[:name], ingredients: params[:ingredients],cook_time: params[:cook_time])
+    recipe_id = @recipe.id.to_s
+    redirect to ('/recipes/' + recipe_id)
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect to ('/recipes')
+  end
 end
---
+++ db/migrate/20200107185145_udpate_cook_time.rb
@@ -0,0 +1,5 @@
+class UdpateCookTime < ActiveRecord::Migration
+  def change
+    change_column :recipes, :cook_time, :string
+  end
+end
